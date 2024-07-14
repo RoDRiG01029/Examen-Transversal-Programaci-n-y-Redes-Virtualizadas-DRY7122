afccfe33ec155bc4d6895444a97c58c3fca83fab
RoDRiG01029 <ro.catalan@duocuc.cl>
2024-07-14 13:57:37 -0400

    Documentos Examen Transversal

---
+++ Examen.py
@@ -0,0 +1,9 @@
+# Nombres_Integrantes
+integrantes = [
+    {"nombre": "Martin", "apellido": "Briones"},
+    {"nombre": "Rodrigo", "apellido": "Catalán"},
+    {"nombre": "Roberto", "apellido": "Hermosilla"}
+]
+
+for integrante in integrantes:
+    print(f"{integrante['nombre']} {integrante['apellido']}")
